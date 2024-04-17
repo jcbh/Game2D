9b461a026ae9cf6d9a41ed69279134372541e97d
Juan Carlos Ballesteros <jcballesteroshermida@gmail.com>
2024-04-17 19:06:03 +0200

    Player walks

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -29,7 +29,7 @@ public class Player extends Entity {
 			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
 			up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
 			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
-			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
 			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
 			left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
 			right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
@@ -48,22 +48,33 @@ public class Player extends Entity {
 	}
 
 	public void update() {
-		if (keyH.upPressed == true) {
-			direction = "up";
-			y -= speed;
+		//This if make that player only walks when a direction is pressed
+		if (keyH.upPressed || keyH.rightPressed || keyH.leftPressed || keyH.downPressed) {
+			if (keyH.upPressed == true) {
+				direction = "up";
+				y -= speed;
+			} else if (keyH.downPressed == true) {
+				direction = "down";
+				y += speed;
+			} else if (keyH.rightPressed == true) {
+				direction = "right";
+				x += speed;
+			} else if (keyH.leftPressed == true) {
+				direction = "left";
+				x -= speed;
+			}
+			spriteCounter++;
+			if (spriteCounter > 12) {
+				if (spriteNum == 1) {
+					spriteNum = 2;
+				} else if (spriteNum == 2) {
+					spriteNum = 1;
+				}
+				spriteCounter = 0;
+
+			}
 		}
-		else if (keyH.downPressed == true) {
-			direction = "down";
-			y += speed;
-		}
-		else if (keyH.rightPressed == true) {
-			direction = "right";
-			x += speed;
-		}
-		else if (keyH.leftPressed == true) {
-			direction = "left";
-			x -= speed;
-		}
+
 	}
 
 	public void draw(Graphics2D g2) {
@@ -75,19 +86,39 @@ public class Player extends Entity {
 		
 		switch(direction) {
 			case "up":{
-				image = up1;
+				if (spriteNum==1) {
+					image = up1;					
+				}
+				if (spriteNum==2) {
+					image = up2;					
+				}
 				break;
 			}
 			case "down":{
-				image = down1;
+				if (spriteNum==1) {
+					image = down1;					
+				}
+				if (spriteNum==2) {
+					image = down2;					
+				}
 				break;
 			}
 			case "left":{
-				image = left1;
+				if (spriteNum==1) {
+					image = left1;					
+				}
+				if (spriteNum==2) {
+					image = left2;					
+				}
 				break;
 			}
 			case "right":{
-				image = right1;
+				if (spriteNum==1) {
+					image = right1;					
+				}
+				if (spriteNum==2) {
+					image = right2;					
+				}
 				break;
 			}
 		}
