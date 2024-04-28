0c0834b5e41e0003cb19911cef85aa12c4922f2a
Juan Carlos Ballesteros <jcballesteroshermida@gmail.com>
2024-04-28 17:52:35 +0200

    avoid black borders

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -92,10 +92,10 @@ public class TileManager {
 			int screenY = worldY - gp.player.worldY + gp.player.screenY;
 			
 			//This if is for paint only the tiles that appears on the screen
-			if (worldX > gp.player.worldX - gp.player.screenX &&
-					worldX < gp.player.worldX + gp.player.screenX &&
-					worldY > gp.player.worldY - gp.player.screenY &&
-					worldY < gp.player.worldY + gp.player.screenY) {
+			if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+					worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+					worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+					worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
 				g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 			}
 			worldCol++;
